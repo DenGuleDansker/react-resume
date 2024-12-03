cbb36b700ec6479921a02a3d93f3e176d401f91e
Chi Linh <chilinhm@gmail.com>
2024-12-03 11:26:28 +0100

    Update References.jsx

--- src/Components/References.jsx
+++ src/Components/References.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const References = ({ label, name, position, mail }) => (
-  <section className="profile section" id="profile">
+  <section className="references section" id="references">
     <h2 className="section-title">{label}</h2>
     <p className="profile__description">{name}</p>
     <p className="profile__description">{position}</p>
